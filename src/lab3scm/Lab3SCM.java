51e8731e3a2e5ff91e695a71359d77a4efdcc74a
g00062625 <g00062625@aus.edu>
2017-09-29 23:08:10 +0400

    Feature A added

--- src/lab3scm/Lab3SCM.java
+++ src/lab3scm/Lab3SCM.java
@@ -1,6 +1,9 @@
 package lab3scm;
 
 public class Lab3SCM {
+    public static void featureA(){
+        System.out.println("Feature A added!");
+    }
     public static void featureB(){
         System.out.println("Feature B added!");
     }
